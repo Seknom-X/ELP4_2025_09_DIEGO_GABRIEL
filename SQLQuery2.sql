62f94d0dc338e3ef91e0b3c2c1b92a8e841acae7
Diego Gabriel <diego.gabriel.foz@gmail.com>
2025-10-01 17:53:34 -0300

    Começo da criação de um banco de daddos para o projeto, iniciando com as tabelas das classes Pais, Estado e Cidade

---
+++ SQLQuery2.sql
@@ -0,0 +1,6 @@
+﻿create table estados(
+	id int not null identity (100, 1)primary key,
+	estado varchar(55) not null,
+	sigla varchar(2) not null,
+	pais_id int not null foreign key references paises(id)
+	);
---
+++ SQLQuery3.sql
@@ -0,0 +1,5 @@
+﻿create table cidades(
+	id int not null identity (1000, 1)primary key,
+	cidade varchar(55) not null,
+	estado_id int not null foreign key references estados(id)
+	);
